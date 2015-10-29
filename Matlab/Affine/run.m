036284d7be46010305ca0d578635fb9c3524a3fe
Richard Teammco <teammco@neuronal.ices.utexas.edu>
2015-10-29 14:30:25 -0500

    Updated (fixed) affine and projective transforms for MATLAB.

--- Matlab/Affine/run.m
+++ Matlab/Affine/run.m
@@ -5,14 +5,14 @@ title('Original Obj');
 
 % Randomly deform the object. Get a rotation, then screw it up by adding
 % random noise.
-R = getRandRotation();
-R = R + (rand(3, 3) / 2);
+%   R = getRandRotation();
+%   R = R + (rand(3, 3) / 2);
 % Deform the object.
-ObjR = rotate(Obj, R);
-t = 10*rand(1, 3);
-ObjR = ObjR - (t' * ones(1, size(ObjR, 1)))';
+%   ObjR = rotate(Obj, R);
+%   t = 10*rand(1, 3);
+%   ObjR = ObjR - (t' * ones(1, size(ObjR, 1)))';
 % Add noise to the object.
-ObjR = ObjR + 0.001 * randn(size(ObjR));
+%   ObjR = ObjR + 0.001 * randn(size(ObjR));
 % Plot it.
 subplot(2,2,2);
 scatter3(ObjR(:,1), ObjR(:,2), ObjR(:,3), 'filled');
